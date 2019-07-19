0685c7abbc64155f6bfb6b04a2c997fe0a45b946
Martin Kröning <m.kroening@hotmail.de>
2019-07-19 10:42:11 +0200

    Upgrade google dependencies

--- CalendarFXGoogle/src/main/java/module-info.java
+++ CalendarFXGoogle/src/main/java/module-info.java
@@ -2,7 +2,7 @@ module com.calendarfx.google {
 
     requires com.calendarfx.view;
     requires javafx.base;
-    requires google.api.services.calendar.v3.rev342;
+    requires com.google.api.services.calendar;
     requires javafx.graphics;
     requires de.jensd.fx.glyphs.fontawesome;
     requires guava.jdk5;
@@ -10,7 +10,7 @@ module com.calendarfx.google {
     requires google.http.client;
     requires javafx.controls;
     requires javafx.web;
-    requires google.api.services.oauth2.v2.rev141;
+    requires com.google.api.services.oauth2;
     requires google.oauth.client;
     requires google.api.client;
     requires google.http.client.jackson2;
