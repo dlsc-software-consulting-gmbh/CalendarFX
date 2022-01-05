03554c9f1bee30e988b9b8d9fa8b7d0d4aecc5fc
Dirk Lemmermann <dlemmermann@gmail.com>
2022-01-05 15:32:11 +0100

    Added sampler launcher class.

---
+++ CalendarFXSampler/src/main/java/com/calendarfx/demo/CalendarFXSampler.java
@@ -0,0 +1,15 @@
+/**
+ * Copyright (C) 2014 - 2021 DLSC Software & Consulting GmbH (dlsc.com)
+ *
+ * This file is part of FlexGanttFX.
+ */
+package com.calendarfx.demo;
+
+import fxsampler.FXSampler;
+
+public class CalendarFXSampler {
+
+    public static void main(String[] args) {
+        FXSampler.main(args);
+    }
+}
